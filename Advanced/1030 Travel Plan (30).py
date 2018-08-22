812b5146a5723b6b3ce49401f27e206b0791ab92
Junjie_Yu <yujunjie@zju.edu.cn>
2018-08-23 02:54:17 +0800

    1030 Travel Plan 单源多权值最短路径问题。不如1018复杂

---
+++ Advanced/1030 Travel Plan (30).py
@@ -0,0 +1,50 @@
+#!/usr/bin/env.python
+# -*- coding:utf-8 -*-
+import heapq
+
+N, M, S, D = map(int, input().split())
+
+table = [[] for i in range(N)]
+
+for _ in range(M):
+    i, j, w, f = map(int, input().split())
+    table[i].append([w, f, j])
+    table[j].append([w, f, i])
+
+def Dijkstra(start, end):
+    inf =float('inf')
+    visited = [0]*N
+    dist = [inf]*N
+    dist[start] = 0
+    fee = [inf]*N
+    fee[start] = 0
+    heap = [(dist[start], start)]
+    path = [None]*N
+    while len(heap)>0:
+        _, i = heapq.heappop(heap)
+        if i == end:
+            break
+        if visited[i]:
+            continue
+        visited[i] = 1
+        for w, f, j in table[i]:
+            if not visited[j] and dist[i] + w <= dist[j]:
+                if dist[i] + w < dist[j]:
+                    dist[j] = dist[i] + w
+                    fee[j] = fee[i] + f
+                    path[j] = i
+                    heapq.heappush(heap, (dist[j], j))
+                elif fee[i] + f <= fee[j]:
+                    fee[j] = fee[i] + f
+                    path[j] = i
+
+    return dist, path, fee
+
+dist, path, fee = Dijkstra(S, D)
+p = D
+road = []
+while p is not None:
+    road.append(p)
+    p = path[p]
+
+print(*road[::-1], dist[D], fee[D])
